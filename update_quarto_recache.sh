d5c6d3688e7235cf1ed83c31117e9ade169f2ba5
John Franchak <franchak@gmail.com>
2026-02-20 13:00:19 -0800

    Automated Quarto update: 2026-02-20 13:00:19

--- update_quarto_recache.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Navigate to your project directory
-# Replace with the absolute path to your project
-cd /Users/johnfranchak/Documents/GitHub/sip_dashboard
-
-# Render the project
-# This assumes 'quarto' is in your system PATH
-/usr/local/bin/quarto render timelines.qmd --cache-refresh
---
+++ update_timelines.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Navigate to your project directory
+# Replace with the absolute path to your project
+cd /Users/johnfranchak/Documents/GitHub/sip_dashboard
+
+# Render the project
+# This assumes 'quarto' is in your system PATH
+/usr/local/bin/Rscript generate_timelines.qmd
+
+# Add changes to Git
+git add .
+
+# Commit with a timestamped message
+git commit -m "Generated timelines: $(date +'%Y-%m-%d %H:%M:%S')"
+
+# Push to GitHub (assumes your 'origin' and 'main' branch are set up)
+git push origin main
